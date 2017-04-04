c0484126f8f02515819dc0fe76eaf87d89bc9923
pushpe <pushpakumara.14@cse.mrt.ac.lk>
2017-04-05 00:50:51 +0530

    new Angular CLI command ara added

--- angular2 CLI commands.php
+++ angular2 CLI commands.php
@@ -5,9 +5,13 @@ npm install typescript -g
 
 ng new {project name}
 
+ng new recipe-book --prefix rb
+
+
 ng serve
 
 ng generate component {component name}
+ng generate directive {directive name}
 
 cd src/app/other/
 ng g c another --flat -is -it
